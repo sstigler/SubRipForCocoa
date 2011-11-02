efe187bcf63efaf4f2fb7e789991794a628e5945
Sam Stigler <sstigler1985@mac.com>
2011-11-01 22:55:29 -0700

    Initial Commit

---
+++ SubRip/SubRip.h
@@ -0,0 +1,13 @@
+//
+//  SubRip.h
+//  SubRip
+//
+//  Created by Sam Stigler on 11/1/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SubRip : NSObject
+
+@end
---
+++ SubRip/SubRip.m
@@ -0,0 +1,23 @@
+//
+//  SubRip.m
+//  SubRip
+//
+//  Created by Sam Stigler on 11/1/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "SubRip.h"
+
+@implementation SubRip
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
