53e76f7ae458f6d9ea1d1ce1708b342175fb988f
Sam Stigler <sstigler1985@mac.com>
2014-05-26 19:35:54 -0500

    Installs Objective-Clean.

--- SubRipTestHarness/SubRipTestHarness/main.m
+++ SubRipTestHarness/SubRipTestHarness/main.m
@@ -8,15 +8,8 @@
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[])
+int main(int argc, const char* argv[])
 {
-
-    @autoreleasepool {
-        
-        // insert code here...
-        NSLog(@"Hello, World!");
-        
-    }
     return 0;
 }
 
